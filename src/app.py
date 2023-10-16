9cfc8096651dee523dadf55501dc8c54d99542ac
Gabriel <parragbriel@gmail.com>
2023-10-16 13:39:24 -0300

    Corregido error que tenia al ejecutarse

--- src/app.py
+++ src/app.py
@@ -3,24 +3,18 @@ import requests
 from flask import Flask, jsonify
 
 app = Flask(__name__)
-
 @app.route('/amazon', methods=["GET"])
+
 def amazon():
+    busqueda = "Televisor" #Recuerda de quitarlo y poner un input
+    url = "https://www.amazon.com/s?k=".format(busqueda)
+    headers = {"FUser": "An","user-agent":"an"}
+    response = requests.get(url,headers=headers)
+
+    if response.status_code==200:
+        soup = BeautifulSoup(response.content, 'html.parser')
 
     return jsonify({"res": "hola"})
 
 if __name__=="__main__":
     app.run(debug=True,port=5000,host="0.0.0.0")
-
-
-busqueda = "Televisor" #Recuerda de quitarlo y poner un input
-url = "https://www.amazon.com/s?k=".format(busqueda)
-
-headers = {"FUser": "An","user-agent":"an"}
-
-r = requests.get(url,headers=headers)
-
-soup = BeautifulSoup(r.content, 'html.parser')
-
-print("hola")
-
