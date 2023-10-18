4c48956898950fef2954ea9dd641d37c4a2cd161
Gabriel <parragbriel@gmail.com>
2023-10-18 14:24:04 -0300

    Ahora obtiene precios

--- testapp.py
+++ testapp.py
@@ -10,34 +10,45 @@ response = requests.get(url,headers=headers)
 
 
 soup = BeautifulSoup(response.content, 'html.parser')
-#--------------------------------NOMBRES PRODUCTOS-----------------------------------------------------------
 
-elemento_nombre = soup.find_all('h2', class_='a-size-mini')
-
-nombre = []
-
-for elemento in elemento_nombre:
-    elementos = elemento.find('span', class_='a-text-normal')
-    nombre.append(elemento.text.strip())
+nombre_clase_producto = 'sg-col-20-of-24 s-result-item s-asin sg-col-0-of-12 sg-col-16-of-20 sg-col s-widget-spacing-small sg-col-12-of-16'
+recorre_productos = soup.find_all('div', class_=nombre_clase_producto)
 
 
 
 #------------------------------------LINK DE PRODUCTOS--------------------------------------------------------
 
-elemento_href = soup.find_all('div', class_='sg-col-20-of-24 s-result-item s-asin sg-col-0-of-12 sg-col-16-of-20 sg-col s-widget-spacing-small sg-col-12-of-16')
-
 href = []
 
-for elemento in elemento_href:
+for elemento in recorre_productos:
 
-    elementos_productos = elemento.find_all('a', class_='a-link-normal s-underline-text s-underline-link-text s-link-style a-text-normal')
+    elementos_href = elemento.find_all('a', class_='a-link-normal s-underline-text s-underline-link-text s-link-style a-text-normal')
     
-    for i in elementos_productos:
+    for i in elementos_href:
         link_href = i.get('href')
         href.append("https://www.amazon.com" + link_href)
 
-for i in href:
+
+#--------------------------------NOMBRES PRODUCTOS-----------------------------------------------------------
+
+nombre = []
+
+for elemento in recorre_productos:
+    elementos_nombres = elemento.find('a', class_='a-link-normal s-underline-text s-underline-link-text s-link-style a-text-normal')
+    if elementos_nombres:
+        nombre.append(elementos_nombres.text.strip())
+
+
+#-------------------------------------PRECIOS------------------------------------------------------------------------
+
+
+precio = []
+
+for elemento in recorre_productos:
+    elementos_precio = elemento.find('span', class_='a-price-whole')
+    elementos_precio_decimal = elemento.find('span', class_='a-price-fraction')
+    if elementos_precio and elementos_precio_decimal:
+        precio.append(elementos_precio.text.strip() + elementos_precio_decimal.text.strip())
+        
+for i in precio:
     print(i)
-
-#-------------------------------------------------------------------------------------------------------------
-
