05044660076869411e8d6771eaac468219d0df3a
Gabriel <parragbriel@gmail.com>
2023-10-18 09:18:50 -0300

    Obtiene los links de los productos

--- src/app.py
+++ src/app.py
@@ -7,14 +7,28 @@ app = Flask(__name__)
 
 def amazon():
     busqueda = "Televisor" #Recuerda de quitarlo y poner un input
-    url = "https://www.amazon.com/s?k=".format(busqueda)
+    url = "https://www.amazon.com/s?k=" + busqueda
     headers = {"FUser": "An","user-agent":"an"}
     response = requests.get(url,headers=headers)
 
     if response.status_code==200:
         soup = BeautifulSoup(response.content, 'html.parser')
+        #//div[@class="s-main-slot s-result-list s-search-results sg-row"]//a[@class="a-link-normal s-no-outline"]
+        #elementos_a = soup.find_all('h2', class_='h3 product-title')
+        href= []
+        urls = soup.find_all('div', class_='a-link-normal s-no-outline')
+        results = []
+        for a_tag in urls:
+            href = a_tag.get('href')
+            text = a_tag.get_text()
+            results.append({"text": text, "href": href})
 
-    return jsonify({"res": "hola"})
+        # Devuelve los resultados como respuesta JSON
+        return jsonify({"results": results})
+
+    return jsonify({"Respuesta": "Fallida "})
+
+   
 
 if __name__=="__main__":
     app.run(debug=True,port=5000,host="0.0.0.0")
--- testapp.py
+++ testapp.py
@@ -1,17 +1,43 @@
 from bs4 import BeautifulSoup
 import requests
+import json
+from flask import Flask, jsonify
 
 busqueda = "Televisor" #Recuerda de quitarlo y poner un input
 url = "https://www.amazon.com/s?k=" + busqueda
 headers = {"FUser": "An","user-agent":"an"}
 response = requests.get(url,headers=headers)
 
-if response.status_code==200:
 
-    soup = BeautifulSoup(response.content, 'html.parser')
+soup = BeautifulSoup(response.content, 'html.parser')
+#--------------------------------NOMBRES PRODUCTOS-----------------------------------------------------------
 
-    elementos = soup.find_all('div', class_='sg-col-inner')
+elemento_nombre = soup.find_all('h2', class_='a-size-mini')
 
-    for i in elementos:
-        hrefs = i.find_all('a', class_='a-link-normal s-no-outline')
-        print(hrefs)
+nombre = []
+
+for elemento in elemento_nombre:
+    elementos = elemento.find('span', class_='a-text-normal')
+    nombre.append(elemento.text.strip())
+
+
+
+#------------------------------------LINK DE PRODUCTOS--------------------------------------------------------
+
+elemento_href = soup.find_all('div', class_='sg-col-20-of-24 s-result-item s-asin sg-col-0-of-12 sg-col-16-of-20 sg-col s-widget-spacing-small sg-col-12-of-16')
+
+href = []
+
+for elemento in elemento_href:
+
+    elementos_productos = elemento.find_all('a', class_='a-link-normal s-underline-text s-underline-link-text s-link-style a-text-normal')
+    
+    for i in elementos_productos:
+        link_href = i.get('href')
+        href.append("https://www.amazon.com" + link_href)
+
+for i in href:
+    print(i)
+
+#-------------------------------------------------------------------------------------------------------------
+
