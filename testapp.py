156ba74558f43065162753963ba003b82cd678ff
Gabriel <parragbriel@gmail.com>
2023-10-19 13:19:30 -0300

    Colocado en una funcion y corregido error que no tomaba precios y dejaba las listas disparejas

--- testapp.py
+++ testapp.py
@@ -5,8 +5,8 @@ import csv
 
 
 
-busqueda = "Televisor" #Recuerda de quitarlo y poner un input
-url = "https://www.amazon.com/s?k=" + busqueda
+busqueda = "telefono" #Recuerda de quitarlo y poner un input
+url = "https://www.amazon.com/s?k=" + busqueda # recuerda hacer que recorra varias paginas
 headers = {"FUser": "An","user-agent":"an"}
 response = requests.get(url,headers=headers)
 
@@ -20,7 +20,7 @@ recorre_productos = soup.find_all('div', class_=nombre_clase_producto)
 
 #------------------------------------LINK DE PRODUCTOS--------------------------------------------------------
 
-href = []
+"""href = []
 
 for elemento in recorre_productos:
 
@@ -30,17 +30,17 @@ for elemento in recorre_productos:
         link_href = i.get('href')
         href.append("https://www.amazon.com" + link_href)
 
-
+"""
 #--------------------------------NOMBRES PRODUCTOS-----------------------------------------------------------
 
-nombre = []
+"""nombre = []
 
 for elemento in recorre_productos:
     elementos_nombres = elemento.find('a', class_='a-link-normal s-underline-text s-underline-link-text s-link-style a-text-normal')
     if elementos_nombres:
         nombre.append(elementos_nombres.text.strip())
 
-
+"""
 #-------------------------------------PRECIOS------------------------------------------------------------------------
 
 precio = []
@@ -50,14 +50,15 @@ for elemento in recorre_productos:
     elementos_precio_decimal = elemento.find('span', class_='a-price-fraction')
     if elementos_precio and elementos_precio_decimal:
         precio.append(elementos_precio.text.strip() + elementos_precio_decimal.text.strip())
-        
 
+for i in precio:
+    print(i)
 #------------------------------------exportar data a csv---------------------------------------------------------------
 
 nombre_archivo = "productos.csv"
 
 # Abre el archivo en modo de escritura
-with open(nombre_archivo, mode='w', newline='') as archivo_csv:
+with open(nombre_archivo, mode='w', newline='', encoding="utf-8") as archivo_csv:
     escritor_csv = csv.writer(archivo_csv)
 
     # Escribe la primera fila con los encabezados
